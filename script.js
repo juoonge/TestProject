bf983faa057e6d47c2e3e7f53f591b41e17d0203
JaeSeo Yang <qud1251@gmail.com>
2022-05-06 16:27:46 +0900

    add exception handling, progress-bar

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 const titleContainer = document.querySelectorAll(".start, .input");
 const titleBtn = document.querySelector(".info")
 const questionContainer = document.querySelector(".question_container");
-const question = document.querySelector('#question');
+const question = document.querySelector('.question_logo');
+const contents = document.querySelector('#question');
 const type = document.querySelector("#type");
 const yBtn = document.querySelector(".answer-1_btn");
 const nBtn = document.querySelector('.answer-2_btn');
 const explain = document.querySelector(".result_show");
 const image = document.querySelector(".result_img");
-const resultContainer = document.querySelector("#result_container");
+const resultContainer = document.querySelector(".end_question");
 const progress = document.querySelector('.progress-bar');
 
 const q = { // question Object 
-    1 : {"title" : "질문 1", "type" : "char", "Y" : "네", "N" : "아니오"},
-    2 : {"title" : "질문 2", "type" : "char", "Y" : "네", "N" : "아니오"},
-    3 : {"title" : "질문 3", "type" : "char", "Y" : "네", "N" : "아니오"},
-    4 : {"title" : "질문 4", "type" : "char", "Y" : "네", "N" : "아니오"},
-    5 : {"title" : "질문 5", "type" : "char", "Y" : "네", "N" : "아니오"},
-    6 : {"title" : "질문 6", "type" : "char", "Y" : "네", "N" : "아니오"},
-    7 : {"title" : "질문 7", "type" : "char", "Y" : "네", "N" : "아니오"},
-    8 : {"title" : "질문 8", "type" : "char", "Y" : "네", "N" : "아니오"},
-    9 : {"title" : "질문 9", "type" : "char", "Y" : "네", "N" : "아니오"},
-    10 : {"title" : "질문 10", "type" : "char", "Y" : "네", "N" : "아니오"},
-    11 : {"title" : "질문 11", "type" : "char", "Y" : "네", "N" : "아니오"},
-    12 : {"title" : "질문 12", "type" : "char", "Y" : "네", "N" : "아니오"},
-    13 : {"title" : "질문 13", "type" : "char", "Y" : "네", "N" : "아니오"}
+    1 : {"title" : "질문 1", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    2 : {"title" : "질문 2", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    3 : {"title" : "질문 3", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    4 : {"title" : "질문 4", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    5 : {"title" : "질문 5", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    6 : {"title" : "질문 6", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    7 : {"title" : "질문 7", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    8 : {"title" : "질문 8", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    9 : {"title" : "질문 9", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    10 : {"title" : "질문 10", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    11 : {"title" : "질문 11", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    12 : {"title" : "질문 12", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"},
+    13 : {"title" : "질문 13", "contents" : "a", "type" : "char", "Y" : "네", "N" : "아니오"}
 }
 
 const result = { // 최종적으로 gender 추가해야 함
@@ -61,12 +62,14 @@ nBtn.addEventListener('click', ()=>{
 
 function updateQuestion() { // Example 1~5 하, 6~10 중, 11~13 상
     if (num == 13) {
-        questionContainer.style.display = "none";
+        questionContainer.style.display = 'none';
         resultContainer.style.display = "block";
         
 
     } else {
         progress.setAttribute('style', `width : calc(100/13*${num}%)`);
+        question.innerHTML = q[num].title;
+        contents.innerHTML = q[num].contents;
         num++;
     }
 }
